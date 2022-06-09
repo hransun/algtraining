decde42067a8ca35391c88a6c14bbd8587917a84
Haoran Sun <haoran.sun@hpe.com>
2022-06-08 20:59:55 -0700

    608

--- 2022/meta/code/08-fortune.py
+++ 2022/meta/code/08-fortune.py
@@ -1,5 +1,16 @@
 import os
 import random
 file = os.popen("wc -l dataset2.csv", 'r').read().split()[0]
+print(os.popen("wc -l dataset2.csv", 'r').read())
 line = random.randint(0, int(file))
-print(line)
+with open('dataset2.csv') as f:
+    count  =0
+    for l in f:
+        if count == int(line):
+            print(l)
+            break
+        else:
+            count +=1
+            
+        
+        
