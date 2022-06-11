1a0757ede5a19c7f528739a8fa96e83c53aecf83
Haoran Sun <haoran.sun@hpe.com>
2022-06-11 15:32:20 -0700

    611

--- 2022/meta/code/12-product_prime.py
+++ 2022/meta/code/12-product_prime.py
@@ -1,10 +1,14 @@
 
 
 def productPrime(nums):
+    # if not nums:
+    #     return []
+    # res = productPrime(nums[1:])
+    # return [nums[0]] + res + [i * nums[0] for i in res]
     if not nums:
         return []
-    res = productPrime(nums[1:])
-    return [nums[0]] + res + [i * nums[0] for i in res]
+    cur = productPrime(nums[1:])
+    return [nums[0]]  + cur + [nums[0] * num for num in cur ]
 
 
 if __name__ == '__main__':
