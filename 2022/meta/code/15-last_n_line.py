3fa54e0f7f831daf8519bc693803a786c5809948
Haoran Sun <haoran.sun@hpe.com>
2022-06-13 21:14:26 -0700

    613

--- 2022/meta/code/15-last_n_line.py
+++ 2022/meta/code/15-last_n_line.py
@@ -1,2 +1,4 @@
 import os
-print(os.popen("tail -2 dataset2.csv", 'r').read())
+# print(os.popen("tail -2 dataset2.csv", 'r').read())
+x = os.popen("tail -2 dataset2.csv", 'r').read()
+print(x)
--- 2022/meta/code/16-word count.py
+++ 2022/meta/code/16-word count.py
@@ -1,27 +1,11 @@
-import csv
-import numpy as np
 import heapq
-g = 9.8
-
-
-def readFromCSV(file_name):
-    d = {}
-    with open(file_name, 'r') as f:
-        f.readline()
-        f_csv = csv.reader(f)
-        for item in f_csv:
-            if file_name == 'dataset2.csv' and item[2] != 'bipedal':
-                continue
-            d[item[0]] = float(item[1])
-    return d
-
 
 def dinosaur():
     word_count = {}
-    with open('1.txt', 'r') as f:
+    with open('dataset1.csv', 'r') as f:
         line = f.readline()
         while line:
-            line = line.split()
+            line = line.rstrip().split(',')
             for word in line:
                 if word in word_count:
                     word_count[word] += 1
@@ -31,6 +15,10 @@ def dinosaur():
     heap = []
     for i in word_count:
         heapq.heappush(heap, (-word_count[i], i))
+    
+    for i in range(3):
+        count, word  = heapq.heappop(heap)
+        print(-int(count), word)
     return heap
 
 
