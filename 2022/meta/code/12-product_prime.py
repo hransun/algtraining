c260d6c5afac13464c413c78a57ca2a9c95b6654
Haoran Sun <haoran.sun@hpe.com>
2022-06-20 11:32:18 -0700

    620

--- 2022/meta/code/12-product_prime.py
+++ 2022/meta/code/12-product_prime.py
@@ -1,5 +1,8 @@
 
-
+# given a set of prime numbers return all possible composite numbers from the given set (given numbers included).
+# For example :
+# Input: [2, 3, 5]
+# Output : [1, 2, 3, 5, 6, 10, 15, 30]
 def productPrime(nums):
     # if not nums:
     #     return []
@@ -8,7 +11,7 @@ def productPrime(nums):
     if not nums:
         return []
     cur = productPrime(nums[1:])
-    return [nums[0]]  + cur + [nums[0] * num for num in cur ]
+    return [nums[0]] + cur + [nums[0] * i for i in cur]
 
 
 if __name__ == '__main__':
