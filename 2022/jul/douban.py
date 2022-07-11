f7f66f9b3d2c1ba2900d647af8bc214b6d8a4291
Haoran Sun <haoran.sun@hpe.com>
2022-07-11 15:45:20 -0700

    711

---
+++ 2022/jul/douban.py
@@ -0,0 +1,12 @@
+import requests
+
+url = 'https://book.douban.com/top250'
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+header = {}
+header['user-agent'] = user_agent
+
+response = requests.get(url,headers=header)
+print(response.text)
+
+from bs4 import BeautifulSoup as bs
+bs(response.text,'html.parser')
